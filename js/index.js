8ecd2ca107ca5b1e26da2f48d0d850656b4e906e
grtj1417 <grtj1417@gmail.com>
2024-05-16 15:16:54 +0800

    Add feature to close dialog on outside click

--- js/index.js
+++ js/index.js
@@ -81,3 +81,12 @@ createBookButton.addEventListener("click", () => {
         dialog.close();
     }
 });
+
+dialog.addEventListener('click', (event) => {
+    const rect = dialog.getBoundingClientRect();
+    const isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+        rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+    if (!isInDialog) {
+        dialog.close();
+    }
+});
